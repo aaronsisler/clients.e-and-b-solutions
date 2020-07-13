37913bf70995faabedff6f68f1f7cae514bf97fd
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-07-12 21:34:23 -0400

    Adding in styling for landing component

--- src/components/about-us/about-us.jsx
+++ src/components/about-us/about-us.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./about-us.scss";
 
-const AboutUs = () => <div className="about-us">AboutUs</div>;
+const AboutUs = () => (
+  <div className="about-us">
+    <h2 className="about-us__title">Who are we?</h2>
+  </div>
+);
 
 export default AboutUs;
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -15,6 +15,17 @@ const Landing = () => (
       title="Let's Get Started"
       className="landing__cta"
     />
+    <div className="landing__section-divide">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
+        <path d="M0,160L80,170.7C160,181,320,203,480,181.3C640,160,800,96,960,64C1120,32,1280,32,1360,32L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+      </svg> */}
+      <svg className="fill" viewBox="0 0 1440 250" preserveAspectRatio="none">
+        <path d="M0,224L80,218.7C160,213,320,203,480,202.7C640,203,800,213,960,186.7C1120,160,1280,96,1360,64L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+      </svg>
+      <svg className="line" viewBox="0 0 1440 250" preserveAspectRatio="none">
+        <path d="M0,224L80,218.7C160,213,320,203,480,202.7C640,203,800,213,960,186.7C1120,160,1280,96,1360,64L1440,32"></path>
+      </svg>
+    </div>
   </div>
 );
 
