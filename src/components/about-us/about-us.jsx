679b2bb3116e5cb11efa200a8f3b35f41c526273
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-07-13 21:24:29 -0400

    Updating the about us section for mobile/desktop

--- src/components/about-us/about-us.jsx
+++ src/components/about-us/about-us.jsx
@@ -5,6 +5,18 @@ import "./about-us.scss";
 const AboutUs = () => (
   <div className="about-us">
     <h2 className="about-us__title">Who are we?</h2>
+    <p className="about-us__text">
+      E&amp;B Solutions was established in 2016 as a challenge to see how ten
+      years of software experience could help small businesses. Reaching out to
+      local businesses showed there was a need for zero maintenance websites.
+      Businesses just wanted to set it and forget it.
+    </p>
+    <p className="about-us__text">
+      The internet of today is very different than the internet of yesterday,
+      and we think the internet of tomorrow is just around the corner. We're
+      writing software to take you all the way from yesterday to today, and then
+      safely to tomorrow and beyond.
+    </p>
   </div>
 );
 
