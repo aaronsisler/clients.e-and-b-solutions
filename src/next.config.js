b1146852bf570f40fb67249baea7a12f47ebde33
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-01-30 21:40:11 -0500

    Removing unused pages and containers

--- src/next.config.js
+++ src/next.config.js
@@ -2,11 +2,6 @@ const withSass = require("@zeit/next-sass");
 
 const nextConfig = {
   distDir: "../.next"
-  // publicRuntimeConfig: {
-  //   SERVICES_API_KEY: process.env.NEXT_PUBLIC_SERVICES_API_KEY
-  //     ? process.env.NEXT_PUBLIC_SERVICES_API_KEY
-  //     : "NOGO"
-  // }
 };
 
 const sassConfig = {
