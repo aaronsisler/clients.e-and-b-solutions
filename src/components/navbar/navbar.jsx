8a68db4562d9d71ac87666f9455f70dfe4545407
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-10-28 21:25:57 -0400

    Slowly adding in support for Nextjs 11

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,7 @@ import Logo from "../logo";
 import Hyperlink from "../../atoms/hyperlink";
 import Hamburger from "../../atoms/hamburger";
 
-import "./navbar.scss";
+import styles from "./navbar.module.scss";
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -20,29 +20,29 @@ const Navbar = () => {
   });
 
   return (
-    <nav className="navbar">
-      <Logo className="navbar__logo" />
-      <div className="navbar__toggle">
+    <nav className={styles.navbar}>
+      <Logo className={styles.navbar__logo} />
+      <div className={styles.navbar__toggle}>
         <Hamburger
           isToggled={isMenuOpen}
           onClick={() => setMenuOpen(!isMenuOpen)}
         />
       </div>
-      <div className={cn("navbar__links", menuToggledClass)}>
+      <div className={cn(styles.navbar__links, menuToggledClass)}>
         <Hyperlink
-          className="navbar__link"
+          className={styles.navbar__link}
           href="/"
           onClick={() => setMenuOpen(false)}
           title="Home"
         />
         <Hyperlink
-          className="navbar__link"
+          className={styles.navbar__link}
           href="/pricing"
           onClick={() => setMenuOpen(false)}
           title="Pricing"
         />
         <Hyperlink
-          className="navbar__link"
+          className={styles.navbar__link}
           href="/contact"
           onClick={() => setMenuOpen(false)}
           title="Contact"
