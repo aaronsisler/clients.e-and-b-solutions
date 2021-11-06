3aa8604d42b2fda96b8a53bc694e7714bb68ca79
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-11-06 18:44:27 -0400

    Working on switching to new deployment process

--- deploy/deployment.js
+++ deploy/deployment.js
@@ -3,12 +3,14 @@ const root = () => {
   const { DEPLOY_ENV } = process.env;
   console.log(DEPLOY_ENV);
 
-  fs.copyFile(`serverless.${DEPLOY_ENV}.yml`, "../serverless.yml", function(
-    err
-  ) {
-    if (err) throw err;
-    console.log("File Renamed!");
-  });
+  fs.copyFile(
+    `./deploy/serverless.${DEPLOY_ENV}.yml`,
+    "serverless.yml",
+    function(err) {
+      if (err) throw err;
+      console.log("File Renamed!");
+    }
+  );
 };
 
 root();
