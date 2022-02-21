c7d360e62c4d46279d53d878bf075e040fdbedf5
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-02-21 17:13:24 -0500

    Updating for changes to CDN

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,18 @@
 import React from "react";
-import App from "next/app";
 import Head from "next/head";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import "../styles/app.scss";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Head>
-          <title>E&amp;B Solutions</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </>
-    );
-  }
-}
-
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>E&amp;B Solutions</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </>
+);
 export default MyApp;
--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PricingContainer from "../containers/pricing-container";
 
-const PricingPage = () => (
-  <main>
-    <PricingContainer />
-  </main>
-);
+const PricingPage = () => {
+  return (
+    <main>
+      <PricingContainer />
+    </main>
+  );
+};
 
 export default PricingPage;
